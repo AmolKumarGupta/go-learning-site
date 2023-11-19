f6d58b8c423ae35e6674a306179ac062d3646e33
amol <amlo1232001@gmail.com>
2023-11-19 23:29:45 +0530

    feat: first page

--- main.go
+++ main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Config struct {
+	Name string
+}
+
+var App Config = Config{Name: "Sample site"}
+
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "index page")
-	})
+	fs := http.FileServer(http.Dir("public/"))
+	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", fs))
 
-	http.ListenAndServe(":8000", r)
+	r.HandleFunc("/", indexPage)
+
+	fmt.Println("site running on http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseGlob("views/*")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	data := struct{ App Config }{App}
+	tmp.ExecuteTemplate(w, "index.html", data)
 }
